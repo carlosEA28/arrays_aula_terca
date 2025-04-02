2726b84f7b80232cce911915ecf18f81c0782cd4
carlosEA28 <carloseaduarte28@gmail.com>
2025-04-02 09:38:38 -0300

    add time inter

--- script.js
+++ script.js
@@ -2,3 +2,40 @@ let ligas = [{}];
 let times = [{}];
 let membros = [{}];
 let elencos = [{}];
+
+const timao = [
+  {
+    Brasileirao_A: {
+      times: {
+        Internacional: {
+          elenco: {
+            goleiro: "Sergio Rochet",
+            lateralEsquerdo: "Bernabei",
+            lateralDireito: "Aguirre",
+            zagueiroEsquerdo: "Gabriel",
+            zagueiroDireito: "Gabriel",
+            volante1: "Thiago Maia",
+            volante2: "Fernando",
+            meiaEsquerdo: "Wesley",
+            meiaCentral: "Alan Patrick",
+            meiaDireito: "Carvalho",
+            atacante: "Borre",
+          },
+        },
+        Gremio: {
+          goleiro: "Thiago Volpi",
+        },
+      },
+    },
+    Brasileirao_B: {
+      times: {
+        elenco: {},
+      },
+    },
+    Copa_do_brasil: {
+      times: {
+        elenco: {},
+      },
+    },
+  },
+];
